88ee1fbde17c750b34fa1c8483d26e128b26fb33
JonLuca DeCaro <jonluca.decaro@gmail.com>
2026-09-20 15:01:25 -0700

    chore: bump version for new TestFlight train

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 import type { ConfigContext, ExpoConfig } from "expo/config";
 
-const VERSION = "1.0.0";
+const VERSION = "1.0.1";
 
 const getConfig = ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
